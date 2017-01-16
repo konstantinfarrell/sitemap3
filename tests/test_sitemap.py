da19020569860e0fc0e83a4fdd10efb723a58bd7
KonstantinFarrell <konstantinfarrell@gmail.com>
2017-01-16 11:59:04 -0800

    Add a test for a bigger sitemap

--- tests/test_sitemap.py
+++ tests/test_sitemap.py
@@ -5,7 +5,12 @@ from sitemap.sitemap import sitemap
 class TestSiteMap(TestCase):
     def setUp(self):
         self.url = 'http://konstantinfarrell.github.io'
+        self.bigger_site = 'https://learnxinyminutes.com'
 
     def test_sitemap(self):
-        results = sitemap(self.url)
+        results = sitemap(self.url, write=False)
+        self.assertIsNotNone(results)
+
+    def test_bigger_sitemap(self):
+        results = sitemap(self.bigger_site, write=False)
         self.assertIsNotNone(results)
