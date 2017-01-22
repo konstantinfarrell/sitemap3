8b3bca590143acdeb025349dca59a2064c138429
KonstantinFarrell <konstantinfarrell@gmail.com>
2017-01-22 14:01:26 -0800

    Finally make it work

--- sitemap/sitemap.py
+++ sitemap/sitemap.py
@@ -1,80 +1,72 @@
 import aiohttp
 import argparse
 import asyncio
-import concurrent
+
 from urllib.parse import urlsplit
 
 from bs4 import BeautifulSoup
 
 
-async def crawl(url, session=None, links=[], not_visited=[]):
-    if not session:
-        with aiohttp.ClientSession() as session:
-            return await crawl_(url, session, links, not_visited)
-    else:
-        return await crawl_(url, session, links, not_visited)
-
-
-async def crawl_(url, session, links, not_visited):
-    if url not in links:
-        links.append(url)
-    links, not_visited = await parse_links(url, session, links, not_visited)
-
-    try:
-        await asyncio.gather(*[asyncio.ensure_future(crawl(link, session, links, not_visited)) for link in not_visited])
-    except ValueError:
-        pass
-
-    return links
-
-
-async def parse_links(url, session, links, not_visited):
-    if url in not_visited:
-        not_visited.remove(url)
-    response = await get_content(url, session)
-    soup = BeautifulSoup(response, 'html.parser')
-    domain = "{0.scheme}://{0.netloc}".format(urlsplit(url))
-    for link in [h.get('href') for h in soup.find_all('a')]:
-        link = clean_link(link, domain)
-        if link is not None:
-            if link not in links:
-                links.append(link)
-                not_visited.append(link)
-                print('{}\t{}'.format(len(not_visited), link))
-    return links, not_visited
-
-
-async def get_content(url, session):
-    async with session.get(url, timeout=60) as response:
-        return (await response.text())
-
-
-def clean_link(link, domain):
-    if link is not None:
-        if link.startswith('/') or link.startswith(domain):
-            if link.startswith('/'):
-                link = '{}{}'.format(domain, link)
-            if '?' in link:
-                link = link.split('?')[0]
-            bad_endings = ['.exe', ]
-            return link
+urls = []
+results = []
 
 
 def sitemap(url, output='sitemap.txt', write=True):
+    urls.append(url) # list of urls to check
+    if len(results) > 0:
+        del results[:]
+
     loop = asyncio.get_event_loop()
     if loop.is_closed():
         loop = asyncio.new_event_loop()
-    #executor = concurrent.futures.ThreadPoolExecutor(5)
-    #loop.set_default_executor(executor)
-    try:
-        loop.run_until_complete(asyncio.gather(*[crawl(url)]))
-    finally:
-        #loop._default_executor.shutdown(wait=True)
-        loop.close()
+
+    loop.run_until_complete(asyncio.ensure_future(crawler(urls, results)))
+
     if write:
         with open(output, 'w') as f:
-            f.write(str('\n'.join(result)))
-    return result
+            f.write(str('\n'.join(results)))
+
+    return results
+
+
+async def crawler(urls, results):
+    while len(urls) > 0:
+        await asyncio.gather(*[asyncio.ensure_future(crawl(url)) for url in urls if url not in results])
+
+
+async def crawl(url):
+    results.append(url)
+    try:
+        urls.remove(url)
+    except ValueError:
+        pass
+
+    async with aiohttp.ClientSession() as session:
+        async with session.request(url=url, method='GET') as response:
+            if response.content_type == 'text/html':
+                content = await response.read()
+                soup = BeautifulSoup(content, 'html.parser')
+                domain = "{0.scheme}://{0.netloc}".format(urlsplit(url))
+                for link in [h.get('href') for h in soup.find_all('a')]:
+                    link = clean_link(link, domain)
+                    if link is not None:
+                        if link not in urls and link not in results:
+                            urls.append(link)
+                            print(link)
+
+def clean_link(link, domain):
+    avoid = ['.exe', '.pdf', ]
+    if link is not None:
+        for a in avoid:
+            if link.endswith(a):
+                return None
+        if not link.startswith('//'):
+            if link.startswith('/') or link.startswith(domain):
+                if link.startswith('/'):
+                    link = '{}{}'.format(domain, link)
+                if '?' in link:
+                    link = link.split('?')[0]
+                return link
 
 
 def main():
--- tests/test_sitemap.py
+++ tests/test_sitemap.py
@@ -1,11 +1,12 @@
 from unittest import TestCase
-from sitemap.sitemap import sitemap
+from sitemap.async_sitemap import sitemap
 
 
 class TestSiteMap(TestCase):
     def setUp(self):
-        self.url = 'http://konstantinfarrell.github.io'
-        self.bigger_site = 'https://learnxinyminutes.com'
+        self.url = 'https://konstantinfarrell.github.io'
+        self.bigger_site = 'https://raspberrypi.org'
+        self.biggest_site = 'https://learnxinyminutes.com'
 
     def test_sitemap(self):
         results = sitemap(self.url, write=False)
@@ -13,4 +14,8 @@ class TestSiteMap(TestCase):
 
     def test_bigger_sitemap(self):
         results = sitemap(self.bigger_site, write=False)
+        self.assertIsNotNone(results)
+
+    def test_biggest_sitemap(self):
+        results = sitemap(self.biggest_site, write=False)
         self.assertIsNotNone(results)
